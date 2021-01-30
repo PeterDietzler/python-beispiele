4b231ab8f779c135928fde6cd6926de32bf52a4d
PeterDietzler <peter.dietzler.home@gmail.com>
2021-01-30 15:25:08 +0100

    add lable

--- Pyside2_GUI/main.py
+++ Pyside2_GUI/main.py
@@ -11,12 +11,17 @@ app = QtWidgets.QApplication(sys.argv)
 
 # Create a Window
 mywindow = QtWidgets.QWidget()
-mywindow.resize(320, 240)
-mywindow.setWindowTitle('Hello, World!')
+mywindow.resize(1000, 700)
+mywindow.setWindowTitle('Hello, World! WindowTitle')
+
+# Create a label and display it all together
+mylabel0 = QtWidgets.QLabel(mywindow)
+mylabel0.setText('Hello, World! 100,100,100,100')
+mylabel0.setGeometry(QtCore.QRect(100, 100, 100, 100))
 
 # Create a label and display it all together
 mylabel = QtWidgets.QLabel(mywindow)
-mylabel.setText('Hello, World!')
+mylabel.setText('Hello, World! 200,200,200,200')
 mylabel.setGeometry(QtCore.QRect(200, 200, 200, 200))
 
 mywindow.show()
