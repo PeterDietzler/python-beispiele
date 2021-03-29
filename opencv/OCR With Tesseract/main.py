a46c07a73fd246ed3e2797c026ac3301bcf9b6e7
dietpte <peter.dietzler@sensitec.com>
2021-03-29 12:15:40 +0200

    test with pycharm

--- opencv/OCR With Tesseract/main.py
+++ opencv/OCR With Tesseract/main.py
@@ -4,15 +4,15 @@ import numpy as np
 import pytesseract
 
 filename = "OCR_picture2.jpg"
+print('stat programm... ')
 
+#pytesseract.pytesseract.tesseract_cmd = r"tesseract"
 
-pytesseract.pytesseract.tesseract_cmd = r"tesseract"
+pytesseract.pytesseract.tesseract_cmd = r"C:\_PortableApps\Tesseract-OCR\tesseract.exe"
 
 img = cv2.imread( filename )
 text = pytesseract.image_to_string(img)
 print(text)
-
-
         
 
 # 1. Load the image
