fb5e8a0cd55cc0cb5ebb04df6bf0b9145bc891bf
dietpte <peter.dietzler@sensitec.com>
2021-03-31 15:43:39 +0200

    add webserver

--- microPython/HalloWelt/readServer.py
+++ microPython/HalloWelt/readServer.py
@@ -1,6 +1,3 @@
-# ************************
-# https://techtotinker.blogspot.com/2020/11/016-esp32-micropython-web-server-esp32.html?m=1
-#
 import machine
 import time
 led = machine.Pin(2,machine.Pin.OUT)
@@ -12,8 +9,7 @@ led.off()
 # as Access Point mode.
 import network
 ssid = 'ESP32-AP-WebServer'
-myPassword ='12345678'
-
+password = '123456789'
 
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
@@ -35,34 +31,36 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('',80)) # specifies that the socket is reachable by any address the machine happens to have
 s.listen(5)     # max of 5 socket connections
 
+
+# DHT sensor initializations
+import dht
+d = dht.DHT22(machine.Pin(23))
+# If you will use DHT11, change it to:
+# d = dht.DHT11(machine.Pin(23))
+
+
 # ************************
 # Function for creating the
 # web page to be displayed
 def web_page():
-    if led.value()==1:
-        led_state = 'ON'
-        print('led is ON')
-    elif led.value()==0:
-        led_state = 'OFF'
-        print('led is OFF')
-        
+    # Get the DHT readings
+    d.measure()
+    t = d.temperature()
+    h = d.humidity()
+    
     html_page = """<!DOCTYPE HTML>  
         <html>  
         <head>  
           <meta name="viewport" content="width=device-width, initial-scale=1">  
+          <meta http-equiv="refresh" content="1">  
         </head>  
         <body>  
            <center><h2>ESP32 Web Server in MicroPython </h2></center>  
-           <center>  
-             <form>  
-               <button type='submit' name="LED" value='1'> LED ON </button>  
-               <button type='submit' name="LED" value='0'> LED OFF </button>  
-             </form>  
-           </center>  
-           <center><p>LED is now <strong>""" + led_state + """</strong>.</p></center>  
+           <center><p>Temperature is <strong>""" + str(t) + """ C.</strong>.</p></center>  
+           <center><p>Humidity is <strong>""" + str(h) + """ %.</strong>.</p></center>  
         </body>  
         </html>"""  
-   return html_page
+   return html_page  
 
 
 while True:
@@ -73,21 +71,12 @@ while True:
     # Socket receive()
     request=conn.recv(1024)
     print("")
-    print("")
     print("Content %s" % str(request))
 
     # Socket send()
     request = str(request)
-    led_on = request.find('/?LED=1')
-    led_off = request.find('/?LED=0')
-    if led_on == 6:
-        print('LED ON')
-        print(str(led_on))
-        led.value(1)
-    elif led_off == 6:
-        print('LED OFF')
-        print(str(led_off))
-        led.value(0)
+    
+    # Create a socket reply
     response = web_page()
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
@@ -96,5 +85,3 @@ while True:
     
     # Socket close()
     conn.close()
-    
-    
