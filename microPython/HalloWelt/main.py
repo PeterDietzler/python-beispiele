cacec9a4367c2ede0106cdee5cf476921a93d0bb
PeterDietzler <peter.dietzler.home@gmail.com>
2021-04-04 11:26:59 +0200

    add Hall and Temperatur measure

--- microPython/HalloWelt/main.py
+++ microPython/HalloWelt/main.py
@@ -3,6 +3,8 @@
 #
 import machine
 import time
+import esp32
+
 led = machine.Pin(2,machine.Pin.OUT)
 led.off()
 
@@ -11,7 +13,7 @@ led.off()
 # Configure the ESP32 wifi
 # as Access Point mode.
 import network
-myssid = 'ESP32-AP-WebServer'
+myssid = 'ESP32-AP-WebServer-001F1F08C4CB' # 00:1F:1F:08:C4:CB'
 mypassword ='12345678'
 
 
@@ -39,21 +41,34 @@ s.listen(5)     # max of 5 socket connections
 # Function for creating the
 # web page to be displayed
 def web_page():
+    espHall = esp32.hall_sensor()     # read the internal hall sensor
+    #espTemperatur = esp32.raw_temperature() # read the internal temperature of the MCU, in Farenheit
+    espTemperatur = (esp32.raw_temperature() - 32) * 5/9
+    
+    
     if led.value()==1:
         led_state = 'ON'
         print('led is ON')
+        R1= 28010
+        R2= 28020
+        R3= 28030
+        R4= 28040
+        R5= 28050
+        R6= 28060
+        R7= 28070
+        R8= 28080
+ 
     elif led.value()==0:
         led_state = 'OFF'
         print('led is OFF')
-    
-    R1= 2800
-    R2= 2800
-    R3= 2800
-    R4= 2800
-    R5= 2800
-    R6= 2800
-    R7= 2800
-    R8= 2800
+        R1= 2801
+        R2= 2802
+        R3= 2803
+        R4= 2804
+        R5= 2805
+        R6= 2806
+        R7= 2807
+        R8= 2808
     
     
     html_page = """<!DOCTYPE HTML>  
@@ -62,21 +77,22 @@ def web_page():
           <meta name="viewport" content="width=device-width, initial-scale=1">  
         </head>  
         <body>  
-           <center><h2>ESP32 Web Server in MicroPython </h2></center>  
+           <center><h2> """ + myssid +     """ </h2></center>  
+           <center><p>Projekt: python-beispiele/microPython/HalloWelt </p></center> 
+           <center><p>Hall       : """ + str(espHall) +     """ mT </p></center> 
+           <center><p>Temperatur : """ + str(espTemperatur) + """ °C</p></center> 
            <center>  
              <form>  
                <button type='submit' name="LED" value='1'> LED ON </button>  
                <button type='submit' name="LED" value='0'> LED OFF </button>  
              </form>  
            </center>  
-           <center><p>R1 = <strong>""" + srt(R1) + """</strong>.</p></center>  
-           <center><p>R2 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R3 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R4 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R5 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R6 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R7 = <strong>""" + led_state + """</strong>.</p></center>  
-           <center><p>R8 = <strong>""" + led_state + """</strong>.</p></center>  
+           <center><p> <strong> Sinus </strong></p></center>  
+           <center><p>R1 = <strong>""" + str(R1) + """</strong> R3 = <strong>""" + str(R3) + """</strong></p></center>  
+           <center><p>R2 = <strong>""" + str(R2) + """</strong> R4 = <strong>""" + str(R4) + """</strong></p></center>  
+           <center><p> <strong> Cosinus </strong></p></center>  
+           <center><p>R5 = <strong>""" + str(R5) + """</strong> R7 = <strong>""" + str(R7) + """</strong></p></center>  
+           <center><p>R6 = <strong>""" + str(R6) + """</strong> R8 = <strong>""" + str(R8) + """</strong></p></center>  
         </body>  
         </html>"""  
     return html_page
