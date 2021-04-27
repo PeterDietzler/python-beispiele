797ce4a1e88409ff1a656cfd8482315579a4ec19
dietpete <Peter.dietzler@sensitec.com>
2021-04-27 21:48:28 +0200

    augereumt

--- Shelly/main.py
+++ Shelly/main.py
@@ -56,80 +56,54 @@ def static_vars(**kwargs):
         return func
     return decorate    
     
-    
-@static_vars(counter1=0)    
-def set_BW_Heizleistung(Leistung): # aktuell freie leistung 
+# EVU_Netz_export ==>> positiv(+) für Netzeinspeisung Netzbezug, negativ(-) für  Netzbezug
+#
+def set_BW_Heizleistung( EVU_Netz_export ): # aktuell freie leistung 
     global Current_State
     global Power_State
     global _Leistung
-    set_BW_Heizleistung.counter1 +=1
-    
-       
-    if not hasattr(set_BW_Heizleistung,"counter3"):
-        set_BW_Heizleistung.counter3=0
-    
-    set_BW_Heizleistung.counter3 += 1 
-    
-    
-    
-    print('counter1', set_BW_Heizleistung.counter1)
-    print('counter3', set_BW_Heizleistung.counter3)
 
-    ip_1KW = "192.168.188.60" # Heizung tempeaturen
-    ip_2KW = "192.168.188.52" # Heizung tempeaturen
+    ip_1KW = "192.168.188.60" # Shelly 1PM Heizung 1kW
+    ip_2KW = "192.168.188.52" # Shelly 1PM Heizung 2kW
     
     Heizstab_1000W = shelly(ip_1KW)
     Heizstab_2000W = shelly(ip_2KW)
 
-    akt_Power_1KW  =  Heizstab_1000W.get_power(0)
-    akt_Power_2KW  =  Heizstab_2000W.get_power(0)
+    akt_Power = Heizstab_1000W.get_power(0) + Heizstab_2000W.get_power(0)
     
-    akt_Power = akt_Power_1KW + akt_Power_2KW
-        
-    
-    Schalt_Leistung = akt_Power_1KW + akt_Power_2KW + Leistung
-    if Leistung < 1:
+    if EVU_Netz_export < 1:
         Schalt_Leistung = akt_Power - 1000 + 200
     else:
-        Schalt_Leistung = akt_Power + Leistung
-    
-    
-    
-    #print('akt_Power_1KW   = ',akt_Power_1KW)
-    #print('akt_Power_2KW   = ',akt_Power_2KW)
-    #print('Leistung        = ',Leistung)
-    #print('Schalt_Leistung = ',Schalt_Leistung)
-    
+        Schalt_Leistung = akt_Power + EVU_Netz_export
     
     if Schalt_Leistung < 1:   # Alles auschalten
         Heizstab_1000W.set_relay(0)
         time.sleep(1)
         Heizstab_2000W.set_relay(0)
-        Power_State = 0
         return 0
+    
     elif Schalt_Leistung < 1000:   # Alles auschalten
         Heizstab_1000W.set_relay(0)
         time.sleep(1)
         Heizstab_2000W.set_relay(0)
-        Power_State = 0
         return 0
+    
     elif Schalt_Leistung < 2000: # 1000W schalten
         Heizstab_1000W.set_relay(1)
         time.sleep(1)
         Heizstab_2000W.set_relay(0)
-        Power_State = 1
         return 1000
+
     elif Schalt_Leistung < 3100: # 2000W schalten
         Heizstab_1000W.set_relay(0)
         time.sleep(1)
         Heizstab_2000W.set_relay(1)
-        Power_State = 2
         return 2000
+    
     elif Schalt_Leistung < 4000: # 3000W schalten
         Heizstab_1000W.set_relay(1)
         time.sleep(1)
         Heizstab_2000W.set_relay(1)
-        Power_State = 3
         return 3000
     else:
         print('Maximal Leisrung 3000W')
@@ -140,7 +114,7 @@ def set_BW_Heizleistung(Leistung): # aktuell freie leistung
 def my_map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-evu_Total_Power         ='http://iobroker01:8087/getPlainValue/node-red.0.EVU.TotalPower'
+evu_Total_Power   ='http://iobroker01:8087/getPlainValue/node-red.0.EVU.TotalPower'
 evu_energie       ='http://iobroker01:8087/getPlainValue/node-red.0.Haus.TotalEnergie'
 evu_ret_energie   ='http://iobroker01:8087/getPlainValue/node-red.0.EVU.EnergieReturned'
 pv_power          ='http://iobroker01:8087/getPlainValue/node-red.0.PV.Power'
@@ -162,13 +136,11 @@ evu = iobroker()
 
 EVU_Netz_exp = 0
 EVU_Netz_exp_alt = 0
-Loop_Counter = 0
 os.environ['TERM'] = 'xterm'
 while True:
     
     clearConsole()
     
-    Loop_Counter += 1
 
     temp            = heitzung.get_temperature( 2)
     PV_Leistung     = evu.get_raw( pv_power)*-1 
@@ -177,50 +149,33 @@ while True:
                                     
     EVU_Netz_Export = EVU_Netz_Bezug * -1.0
     
-    # EVU_Netz_exp ist der gdämpte export 
-    EVU_Netz_exp =  EVU_Netz_exp_alt *0.7 + EVU_Netz_Export*0.3    
+    # EVU_Netz_exp ist der gdämpte export( Filter 1er Ordnung) 
+    EVU_Netz_exp =  EVU_Netz_exp_alt *0.9 + EVU_Netz_Export*0.1    
     EVU_Netz_exp_alt= EVU_Netz_exp
     
-    
-    #if EVU_Netz_exp < 0:
-    #    EVU_Netz_exp = 0    
-    evu_Netz_Bezug = EVU_Netz_Bezug
-    '''
-    if EVU_Netz_Bezug < 0 :
-        evu_Netz_Bezug = 0
-    else:
-        evu_Netz_Bezug = EVU_Netz_Bezug
-    '''
-    evu_Netz_Export = EVU_Netz_Export
-    '''
-    if EVU_Netz_Export < 0 :
-        evu_Netz_Export = 0
-    else: evu_Netz_Export = EVU_Netz_Export
-    '''
     if (BW_Speicher_soc < 100):
         Speicher_Lade_Leistung = set_BW_Heizleistung( EVU_Netz_exp )
     
     if (BW_Speicher_soc >= 100) :
         Speicher_Lade_Leistung = set_BW_Heizleistung( 0 )
-  
 
     # seconds passed since epoch
     seconds = time.time()
     local_time = time.ctime(seconds)
     print('Warm-Wasser-Speicher PV-Überschuß Ladereglung ' )    
-    print(local_time, '(loop=%d)' % Loop_Counter)    
+    print(local_time)    
     print('-------------------------------------------------------------' )
-    print("|     PV ges.   : %4.0dW      |    Netz_exp.   : %4.0f W" % (PV_Leistung, evu_Netz_Export ))
+    print("|     PV ges.   : %4.0dW      |    Netz_exp.   : %4.0fW" % (PV_Leistung, EVU_Netz_Export ))
     print('-------------------------------------------------------------' )
-    print("|     Haus verb.: %4.0dW      |    Netz_imp.   : %4.0f W" % (PV_Leistung + EVU_Netz_Bezug-Speicher_Lade_Leistung , evu_Netz_Bezug ))
+    print("|     Haus verb.: %4.0dW      |    Netz_imp.   : %4.0fW" % (PV_Leistung + EVU_Netz_Bezug-Speicher_Lade_Leistung , EVU_Netz_Bezug ))
     print('-------------------------------------------------------------' )
-    print("|     WW Power  : %4.0dW      |    Netz(Filter): %4.0f W" % (Speicher_Lade_Leistung, EVU_Netz_exp))
+    print("|     WW Power  : %4.0dW      |    Netz(Filter): %4.0fW" % (Speicher_Lade_Leistung, EVU_Netz_exp))
     print('-------------------------------------------------------------' )
-    print("|     WW SoC    : %2.0d %%       |    WW Temp     : %2.2f °C" % (BW_Speicher_soc, temp))
+    print("|     WW SoC    : %2.0d%%       |    WW Temp     : %2.2f°C" % (BW_Speicher_soc, temp))
     print('-------------------------------------------------------------' )
     print('' )
 
-    time.sleep(10)
+    time.sleep(2)
 
 
 
